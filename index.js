16b7e5ad0982c714bfdd950a87a4d735ea4ec18f
Diana Thayer <me@dianathayer.org>
2015-05-24 15:08:00 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var path = require('path');
+
+function get_training_images (n, m) {
+
+}
+
+function get_testing_images (n, m) {
+  
+}
+
+module.exports = {};
---
+++ test/index.js
@@ -0,0 +1,11 @@
+var mnist = require('../');
+
+describe('mnist', function () {
+  before(function () {
+
+  });
+
+  it('should retrieve ranges of images as lists of [i][j] matrices of integers between 0 and 255 inclusive', function () {
+    // TODO
+  });
+});
